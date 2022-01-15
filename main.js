9efa09667ee5341f14b1634d175a08ac613a52e3
canodelacuadra <canodelacuadra@gmail.com>
2022-01-15 18:16:00 +0100

    correcciones

--- main.js
+++ main.js
@@ -6,46 +6,46 @@ var catedral = document.getElementById("catedral");
 var ayuntamiento = document.getElementById("ayuntamiento");
 // hacemos que desaparezcan todos menos cuenca
 cuenca.style.display = "block";
-
 casco.style.display = "none";
 casas.style.display = "none";
 catedral.style.display = "none";
 ayuntamiento.style.display = "none";
-//
-cuenca.style.animation = "opacity-normal";
-cuenca.style.animationDuration = "3s";
+//hacemos que la promera diapositiva tenga un efecto
+cuenca.style.animation = "opacity-normal 5s";
+cuenca.firstElementChild.style.animation = "top-normal 5s  ";
 //lanzamos eventos y animaciones sobre cuenca
 cuenca.addEventListener("click", function () {
   cuenca.style.display = "none";
   casco.style.display = "block";
-  casco.style.animation = "top-normal";
-  casco.style.animationDuration = "1s";
+  casco.style.animation = "right-normal 5s";
+  casco.firstElementChild.style.animation = "bottom-normal 10s  ";
 });
 //lanzamos eventos sobre casco
 casco.addEventListener("click", function () {
   casco.style.display = "none";
   casas.style.display = "block";
-  casas.style.animation = "right-normal";
-  casas.style.animationDuration = "1s";
+  casas.style.animation = "right-normal 5s";
+  casas.firstElementChild.style.animation = "top-normal 10s  ";
 });
 //lanzamos eventos sobre casas
 casas.addEventListener("click", function () {
   casas.style.display = "none";
   catedral.style.display = "block";
-  catedral.style.animation = "left-normal";
-  catedral.style.animationDuration = "1s";
+  catedral.style.animation = "left-normal 5s";
+  catedral.firstElementChild.style.animation = "opacity-normal 10s  ";
 });
 //lanzamos eventos sobre catedral
 catedral.addEventListener("click", function () {
   catedral.style.display = "none";
   ayuntamiento.style.display = "block";
-  ayuntamiento.style.animation = "bottom-normal";
-  ayuntamiento.style.animationDuration = "1s";
+  ayuntamiento.style.animation = "bottom-normal 5s";
+  ayuntamiento.firstElementChild.style.animation = "right-normal 10s  ";
 });
 //lanzamos eventos sobre ayuntamiento
 ayuntamiento.addEventListener("click", function () {
   ayuntamiento.style.display = "none";
   cuenca.style.display = "block";
-  cuenca.style.animation = "skew-normal";
-  cuenca.style.animationDuration = "1s";
+  cuenca.style.animation = "skew-normal 5s";
+  cuenca.firstElementChild.style.animation = "opacity-normal 10s  ";
 });
+
--- readme.md
+++ readme.md
@@ -1,15 +1,20 @@
 # Escenas Animadas con CSS y JS
 ## Histórica ciudad amurallada de Cuenca
-Debemos hacer que cinco escenas (presentación +4) se sucendan en pantalla mediante javascript
-Cada escena tiene sus propios textos y su imagen
-Las escenas deben ocupar el total del viewport
-Nos ayudamos con la librería bootstrap
-Cada escena debe contener texto, imagen de fondo, y un icono
-Cada escena entra con una animación CSS
-Cambiamos de escena tocando la tecla espacio,  o la tecla intro
+1. Debemos hacer que cinco escenas (presentación +4) se sucedan en pantalla mediante javascript
+2. Cada escena tiene sus propios textos y su imagen
+3. Las escenas deben ocupar el total del viewport
+4. Nos ayudamos con la librería bootstrap
+5. Cada escena debe contener texto, imagen de fondo, y un strong
+6. Cada escena entra con una animación CSS general y otra para los textos
+7. Cambiamos de escena tocando haciendo click, la tecla espacio,  o la tecla intro
 ## Pasos
 1. Plantilla de inicio con bootstrap, iconos bootstrap, main.js y style.css
 2. Creamos el html con 5 escenas identificadas con un id
-3. Colocamos los textos. titulo y párrafos (la imagen la colocaremos como fondo de cada escena)
+3. Colocamos los textos. titulo y párrafos (con un strong al menos en cada uno) (la imagen la colocaremos como fondo de cada escena)
 4. Optimizamos las imágenes a 	1280 x 720
 5. Damos estilo a la primera escena
+6. Damos estilo al resto
+7. Creamos la sucesión de escenas con javascript con click
+8. Aplicamos animaciones a las escenas mediante css y js
+9. Aplicamos animaciones a los textos mediante css y js
+10. Hacemos una función y creamos eventos para barra de espacio o tecla intro
